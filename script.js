5a4e5f058608f604d9e17116f0d73d4041a02c59
Angelika Sowka <sowkaangelika@gmail.com>
2024-12-13 07:48:32 -0500

    Form validation

--- script.js
+++ script.js
@@ -3,27 +3,56 @@ const password = document.getElementById("password");
 const fname = document.getElementById("fname");
 const lname = document.getElementById("lname");
 const email = document.getElementById("email");
+const form = document.getElementById("form");
+const referralSource = document.getElementById("referral-source");
+const message = document.getElementById("message");
 
-document.getElementById("form").addEventListener("submit", (event) => {
-  event.preventDefault();
-  setLocalStorageItem(
-    "userDetails",
-    "username",
-    username.value,
-    "password",
-    password.value,
-    "fname",
-    fname.value,
-    "lname",
-    lname.value,
-    "email",
-    email.value
-  );
+const usernameError = document.getElementById("username-error");
+const passwordError = document.getElementById("password-error");
+const emailError = document.getElementById("email-error");
+
+form.addEventListener("submit", (event) => {
+  if (email.value === "" || email.value == null) {
+    event.preventDefault();
+    emailError.innerHTML = "Email is required!";
+    email.style.border = "1px solid red";
+  }
+
+  if (username.value === "" || username.value == null) {
+    event.preventDefault();
+    usernameError.innerHTML = "Username is required!";
+    username.style.border = "1px solid red";
+  }
+
+  if (password.value === "" || password.value == null) {
+    event.preventDefault();
+    passwordError.innerHTML = "Password is required!";
+    password.style.border = "1px solid red";
+  } else
+    setLocalStorageItem(
+      "userDetails",
+      "username",
+      username.value,
+      "password",
+      password.value,
+      "fname",
+      fname.value,
+      "lname",
+      lname.value,
+      "email",
+      email.value,
+      "referral-source",
+      referralSource.value,
+      "message",
+      message.value
+    );
   username.value = "";
   password.value = "";
   fname.value = "";
   lname.value = "";
   email.value = "";
+  referralSource.value = "Select";
+  message.value = "";
 });
 
 function setLocalStorageItem(
@@ -37,7 +66,11 @@ function setLocalStorageItem(
   key4,
   val4,
   key5,
-  val5
+  val5,
+  key6,
+  val6,
+  key7,
+  val7
 ) {
   localStorage.setItem(
     targetStorageItemName,
@@ -47,7 +80,8 @@ function setLocalStorageItem(
       [key3]: val3,
       [key4]: val4,
       [key5]: val5,
+      [key6]: val6,
+      [key7]: val7,
     })
   );
-  console.log("new storage item saved");
 }
